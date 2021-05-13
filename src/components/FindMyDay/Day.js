ff471fd51ee309168b285c06eea2c6856c318cac
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-05-13 12:46:28 +0600

    update

--- src/components/FindMyDay/Day.js
+++ src/components/FindMyDay/Day.js
@@ -3,9 +3,14 @@ import React from 'react';
 const Day = (props) => {
     const {date, namaj, priorityWork1, priorityWork2, priorityWork3, workDone, valoKaj, ayat, hadith, motivation, idea, gonah, email} = props.day;
     return (
-        <div>
+        <div className="container">
             <h1>{date}</h1>
             <p style={{color: 'red'}}>{email}</p>
+            <ul>
+                <li>{priorityWork1}</li>
+                <li>{priorityWork2}</li>
+                <li>{priorityWork3}</li>
+            </ul>
         </div>
     );
 };
